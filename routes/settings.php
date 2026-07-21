9df36391c803f8b5a3160c45abf9bf3ad55d0998
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-07-21 17:24:52 +0300

    install rector, add composer command and run format

--- routes/settings.php
+++ routes/settings.php
@@ -21,9 +21,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('security.edit');
 });
 
-Route::get('.well-known/passkey-endpoints', function () {
-    return response()->json([
-        'enroll' => route('security.edit'),
-        'manage' => route('security.edit'),
-    ]);
-})->name('well-known.passkeys');
+Route::get('.well-known/passkey-endpoints', fn () => response()->json([
+    'enroll' => route('security.edit'),
+    'manage' => route('security.edit'),
+]))->name('well-known.passkeys');
