4f9434deb4885857d14d36505eb33d9814929ca1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-07-22 13:36:13 +0300

    add create button on index page

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -1,16 +1,22 @@
 <x-layouts::app :title="__('Records')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <flux:heading size="xl" level="1">Good {{ $partOfDay }}, {{ auth()->user()->name }}</flux:heading>
-        <flux:text class="mt-2 mb-6 text-base">Here is what's new in your records today,
-            {{ now()->format('l, F j, Y') }}
-        </flux:text>
+        <div>
+            <flux:text class="mt-2 mb-6 text-base">Here is what's new in your records today,
+                {{ now()->format('l, F j, Y') }}
+            </flux:text>
+            <flux:button href="{{ route('records.create') }}" variant="primary" color="lime" icon="plus-circle">
+                Add new record
+            </flux:button>
+        </div>
+
 
         <flux:separator variant="subtle" />
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             @forelse ($records as $record)
                 <flux:callout icon="pencil-square" color="lime" inline>
                     <flux:callout.heading>{{ $record->name }}</flux:callout.heading>
-                    <flux:badge icon="clock" size="lg">{{ $record->date_time->translatedFormat('d F Y l H:i') }}
+                    <flux:badge icon="clock" size="sm">{{ $record->date_time->translatedFormat('d F Y l H:i') }}
                     </flux:badge>
 
                 </flux:callout>
